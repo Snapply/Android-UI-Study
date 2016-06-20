2e808357539520ae8764dda14e2fb3b8a030ecb4
Snapply <350624690@qq.com>
2016-06-20 09:52:51 +0800

    TableLayout简易界面--增加判断

--- app/src/main/java/free/uilayout_to_github/FirstActivity.java
+++ app/src/main/java/free/uilayout_to_github/FirstActivity.java
@@ -2,16 +2,35 @@ package free.uilayout_to_github;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by luweiling on 2016/6/20.
  */
 public class FirstActivity extends Activity {
+    private EditText account;
+    private EditText password;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.first_layout);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String accountstr = account.getText().toString();
+                String passwordstr = password.getText().toString();
+                if(accountstr.equals("test")){
+                    if(passwordstr.equals("123")){
+                        Toast.makeText(FirstActivity.this, "Access Success", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
